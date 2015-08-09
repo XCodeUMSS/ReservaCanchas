d20ba96359c126d05611a2145ceeb3946a80a340
beimarhuarachi <beimar020@gmail.com>
2015-08-08 21:58:27 -0400

    vista para ver las reservas realizadas agregadas a la vista principal

---
+++ application/views/tabla_reserva.php
@@ -0,0 +1,28 @@
+<div class="col-md-12">
+    <h2>Lista de Reservas</h2>
+    <div class="table-responsive">
+        <table id="tabla_canchas" class="table table-bordered table-striped">
+            <thead>
+                <tr>
+                    <th>Nombre</th>
+                    <th>Telefono de Referencia</th>
+                    <th>Campo Deportivo</th>
+                    <th>Fecha Reserva</th> 
+                    <th>Hora Inicio</th>
+                    <th>Hora Fin</th>
+                </tr>
+            </thead>
+            <tbody id="cuerpo-tabla-reservas">
+                <tr>
+                    <td>Algun Nombre</td>
+                    <td>Algun telefono</td>
+                    <td>Algun campo</td>
+                    <td>Alguna fecha</td>
+                    <td>Alguna hora</td>
+                    <td>Alguna Hora</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
+
