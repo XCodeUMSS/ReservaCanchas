790ca8118f703e53d72eb90cc67a5189415bc971
alison <fernablanco.alison@gmail.com>
2015-08-09 16:53:56 -0400

    Mejorando validacion datos form

--- application/controllers/ControladorReserva.php
+++ application/controllers/ControladorReserva.php
@@ -4,13 +4,17 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 /**
  * Esta clase realizara todo lo relacionado con las reservas
  *
- * @author Beimar
+ * @author Beimar & Alison
  */
 class ControladorReserva extends CI_Controller {
+    
+    private $validador;
     
     public function __construct() {
         parent::__construct();
         $this->load->model('consultas');
+        include(APPPATH.'controllers/ValidadorDatos.php');
+        $this->validador = new ValidadorDatos();
     }
     
     public function index() {
@@ -23,11 +27,12 @@ class ControladorReserva extends CI_Controller {
         $nombre = $this->input->post('nombre_cliente');
         $telefono = $this->input->post('telefono_referencia');
         $idCampo = $this->input->post('campo_deportivo');
-        $fecha = $this->input->post('fecha_reserva');
+        $fecha = date_format(date_create($this->input->post('fecha_reserva')), 
+                "d/m/Y");
         $horaInicio = $this->input->post('hora_inicio').":00";
         $horaFin = $this->input->post('hora_fin').":00";
-
-        if($this->datosValidos($nombre, $idCampo, $fecha, $horaInicio, $horaFin)){
+        
+        if($this->realizarReserva($nombre, $idCampo, $fecha, $horaInicio, $horaFin)){
             $precio = $this->calcularPrecio($idCampo, $horaInicio, $horaFin);
             $reserva = array(
                 'NombreCliente' => $nombre,
@@ -43,27 +48,18 @@ class ControladorReserva extends CI_Controller {
         $this->index();
     }
     
-    public function datosValidos($nombre, $idCampo, $fecha, $horaInicio, $horaFin){
-        $valido = true;
+    public function realizarReserva($nombre, $idCampo, $fecha, $horaInicio, 
+            $horaFin){
         $mensaje = "";
-        if(trim($nombre) == ""){
-            $mensaje .= "- Nombre no valido ";
-            $valido = false;
-        }
-        if($this->existeReserva($idCampo, $fecha, $horaInicio, $horaFin)){
-            $mensaje .= "- Existe una reserva. ";
-            $valido = false; 
-        }
-        if ($horaInicio >= $horaFin) {
-            $mensaje .= "- La hora de inicio es mayor o igual"
-                    . "que la hora final. ";
-            $valido = false;
-        }
-        if(!$this->dentroHorariosAtencion($idCampo, $horaInicio, $horaFin)){
-            $mensaje .= "- Las horas no estan dentro de los "
-                    . "horarios de atencion. ";
-            $valido = FALSE;
-        }
+        $mensaje .= $this->validador->datosValidosReserva($nombre, 
+                $fecha, $horaInicio, $horaFin);
+        $mensaje .= $this->existeReserva($idCampo, $fecha, $horaInicio, 
+                $horaFin);
+        $mensaje .= $this->dentroHorariosAtencion($idCampo, $horaInicio, 
+                $horaFin);
+        
+        $valido = $mensaje == "";
+        
         if(!$valido){
             echo '<script>alert("'.$mensaje.'");</script>';
         }
@@ -75,12 +71,13 @@ class ControladorReserva extends CI_Controller {
         $horarioIni = $horarios->HoraInicio;
         $horarioFin = $horarios->HoraFin;
         return $horaInicio >= $horarioIni && $horaInicio < $horarioFin &&
-                $horaFin > $horarioIni && $horaFin <= $horarioFin;
+                $horaFin > $horarioIni && $horaFin <= $horarioFin ? "" : "- Las"
+                . " horas no estan dentro de los horarios de atencion. ";
     }
 
     public function existeReserva($idCampo, $fecha, $horaInicio, $horaFin) {
         return $this->consultas->existeReserva($idCampo, $fecha, $horaInicio, 
-                $horaFin);
+                $horaFin) ? "- Existe una reserva." : "";
     }
 
     public function calcularPrecio($idCampo, $horaInicio, $horaFin) {
@@ -93,5 +90,4 @@ class ControladorReserva extends CI_Controller {
         $precio = $precioPorHora * $diferencia / 60;
         return $precio;
     }
-
 }
