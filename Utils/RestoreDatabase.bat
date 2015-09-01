d7d535f7e7d5270cee8279ebede240018a77bb3f
Walter Antonio Ramirez Cossio <antonio200x@gmail.com>
2015-09-01 19:04:22 -0400

    Updating RestoreDatabase
    Adding postgresql database password in order to avoid prompts during the execution of the batch file

--- Utils/RestoreDatabase.bat
+++ Utils/RestoreDatabase.bat
@@ -1,3 +1,4 @@
 net stop Apache2.4
-cmd /c psql -h localhost -U postgres -W -f %~d0%~p0..\DB\ReservaCanchasDB.sql
+set PGPASSWORD=postgresql
+cmd /c psql -h localhost -U postgres -d postgres -p 5432 -f %~d0%~p0..\DB\ReservaCanchasDB.sql
 net start Apache2.4
